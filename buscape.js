7b38613fcbf3e1d03e5dd4790c092cc0fb0bac5a
Luan Pontolio <luan.pontolio@gmail.com>
2017-04-27 18:09:30 -0300

    fixes again.

--- buscape.js
+++ buscape.js
@@ -3,7 +3,7 @@
   var html       = document.querySelector('html');
   // var customerId = html.getAttribute('data-session');
   var customerId = "asahshajhsj"
-  var storeId    = document.scripts["mltplace_tray"].src.replace(/.*?\?storeId=/, "")
+  var storeId    = document.scripts["mktplace"].src.replace(/.*?\?storeId=/, "")
 
   if(customerId){
     var layer = dataLayer[0];
--- generate_script.js
+++ generate_script.js
@@ -1,34 +1,9 @@
-// function buscape() {
-
-//   var html       = document.querySelector('html');
-//   var customerId = "asahshajhsj"
-//   var storeId = html.getAttribute('data-store');
-
-//   if(customerId){
-//     var layer = dataLayer[0];
-
-//     if (layer.pageCategory != 'Carrinho'){
-//       return;
-
-//     } else {
-//       var iframeUrl = "https://o.lomadee.com/loc/session/";
-
-//       var iframe         = document.createElement("iframe");
-//       iframe.src         = iframeUrl + storeId + "?customerid=" + customerId;
-//       iframe.width       = 0;
-//       iframe.height      = 0;
-
-//       document.body.appendChild(iframe);
-//     }
-//   }
-// };
-
 (function(d, s, id) {
   var js, script = d.getElementsByTagName(s)[0];
 
   if (d.getElementById(id)) return;
 
-  var storeId = d.querySelector('html').getAttribute('data-store');
+  var storeId = script.src.replace(/.*?\?storeId=/, "");
 
   js        = d.createElement(s);
   js.id     = id;
@@ -36,5 +11,4 @@
   js.src    = "//luanpontolio.github.io/buscape.js?storeId=" + storeId;
   script.parentNode.insertBefore(js, script);
 
-}(document, 'script', 'mltplace_tray'));
-
+}(document, 'script', 'mktplace'));
