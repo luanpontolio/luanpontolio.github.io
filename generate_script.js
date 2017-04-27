97571ae0840d78fdeed7cf54b6fce1157978ca8c
Luan Pontolio <luan.pontolio@gmail.com>
2017-04-27 15:44:58 -0300

    new changes on script.

--- generate_script.js
+++ generate_script.js
@@ -1,41 +1,44 @@
-(function(window){
+function buscape() {
+  'use strict';
+  var html       = document.querySelector('html');
+  // var customerId = html.getAttribute('data-session');
+  var customerId = "asahshajhsj"
+  var storeId    = html.getAttribute('data-store');
 
-	function buscape() {
-	  'use strict';
-	  var html       = document.querySelector('html');
-	  // var customerId = html.getAttribute('data-session');
-	  var customerId = "asahshajhsj"
-	  var storeId    = html.getAttribute('data-store');
+  if(customerId){
+    var layer = dataLayer[0];
 
-	  if(customerId){
-	    var layer = dataLayer[0];
+    if (layer.pageCategory != 'Carrinho'){
+      return;
 
-	    if (layer.pageCategory != 'Carrinho'){
-	      return;
+    } else {
+      var iframeUrl = "https://o.lomadee.com/loc/session/";
 
-	    } else {
-	      var iframeUrl = "https://o.lomadee.com/loc/session/";
+      var iframe         = document.createElement("iframe");
+      iframe.src         = iframeUrl + storeId + "?customerid=" + customerId;
+      iframe.width       = 0;
+      iframe.height      = 0;
+      iframe.frameheight = 0;
+      iframe.frameborder = 0;
 
-	      var iframe         = document.createElement("iframe");
-	      iframe.src         = iframeUrl + storeId + "?customerid=" + customerId;
-	      iframe.width       = 0;
-	      iframe.height      = 0;
-	      iframe.frameheight = 0;
-	      iframe.frameborder = 0;
+      document.body.appendChild(iframe);
+    }
+  }
+};
 
-	      document.body.appendChild(iframe);
-	    }
-	  }
-	}
+(function(d, s, id) {
+  var js, script = d.getElementsByTagName(s)[0];
 
-	var a    = document.createElement("script");
-	a.type   = "text/javascript";
-	a.async  = true;
-	a.id     = "mktplace_tray";
-	a.src    = "#";
-	a.onload = this.buscape;
-	var b    = document.getElementsByTagName("script")[0];
+  if (d.getElementById(id)) return;
 
-	b.parentNode.insertBefore(a,b);
-}(window));
+  var storeId = d.querySelector('html').getAttribute('data-store');
+
+  js        = d.createElement(s);
+  js.id     = id;
+  js.async = true;
+  js.src    = "#";
+  js.onload = this.buscape;
+  script.parentNode.insertBefore(js, script);
+
+}(document, 'script', 'mltplace_tray'));
 
