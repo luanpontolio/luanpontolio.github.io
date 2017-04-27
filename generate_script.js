2367b77a0fd5388c0e3ef018b1b88bc0ab373e19
Luan Pontolio <luan.pontolio@gmail.com>
2017-04-27 08:29:32 -0300

    fixes script

--- generate_script.js
+++ generate_script.js
@@ -3,7 +3,7 @@
   a.type  = "text/javascript";
   a.async = true;
   a.id    = "mktplace_tray";
-  a.src   = "http://images.tcdn.com.br/406267/themes/240/js/buscape.js";
+  a.src   = "buscape.js";
   var b   = document.getElementsByTagName("script")[0];
   b.parentNode.insertBefore(a,b)
 })();
