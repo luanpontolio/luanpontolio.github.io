607b0f717f57338ff3a5d6602d840af06a8c88e7
Luan Pontolio <luan.pontolio@gmail.com>
2017-04-27 14:27:37 -0300

    change script.

--- generate_script.js
+++ generate_script.js
@@ -1,9 +1,46 @@
-(function(){
-  var a   = document.createElement("script");
-  a.type  = "text/javascript";
-  a.async = true;
-  a.id    = "mktplace_tray";
-  a.src   = "https://luanpontolio.github.io/buscape.js";
-  var b   = document.getElementsByTagName("script")[0];
+function buscape() {
+  'use strict';
+  var html       = document.querySelector('html');
+  // var customerId = html.getAttribute('data-session');
+  var customerId = "asahshajhsj"
+  var storeId    = html.getAttribute('data-store');
+
+  if(customerId){
+    var layer = dataLayer[0];
+
+    if (layer.pageCategory != 'Carrinho'){
+      return;
+
+    } else {
+      var iframeUrl = "https://o.lomadee.com/loc/session/";
+
+      var iframe         = document.createElement("iframe");
+      iframe.src         = iframeUrl + storeId + "?customerid=" + customerId;
+      iframe.width       = 0;
+      iframe.height      = 0;
+      iframe.frameheight = 0;
+      iframe.frameborder = 0;
+
+      document.body.appendChild(iframe);
+    }
+  }
+}
+
+function loadScript() {
+  var script = document.scripts
+  var url    = script["teste"];
+  var params = url.src.replace(/.*\?params=/, "");
+  alert(params);
+
+  var a    = document.createElement("script");
+  a.type   = "text/javascript";
+  a.async  = true;
+  a.id     = "mktplace_tray";
+  a.src    = "script.js";
+  a.onload = this.buscape();
+  var b    = document.getElementsByTagName("script")[0];
+
   b.parentNode.insertBefore(a,b)
-})();
+}
+
+window.load = loadScript();
