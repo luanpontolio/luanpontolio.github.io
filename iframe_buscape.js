fab7c26ee59cb3aa04ffbd82b5fb758b39e2305f
Luan Pontolio <luan.pontolio@gmail.com>
2017-06-01 16:12:01 -0300

    fixes script

--- iframe_buscape.js
+++ iframe_buscape.js
@@ -36,35 +36,37 @@
 
 // executeIframe = window.setInterval( function() { verifyDataLayer }, 3000 );
 
-$(document).ready(function(){
-  var verifyDataLayer = function() {
-    if(typeof dataLayer === "undefined"){
-      return;
-    }
-
-    var customerId = document.querySelector("html").getAttribute("data-session");
-    var storeId    = document.scripts["mktplace"].getAttribute("data-store");
-
-    if(customerId){
-      var layer  = dataLayer[0];
-      var iframe = document.getElementById('iframe_mktplace');
-      if(iframe == null && layer.pageCategory === 'Carrinho'){
-        clearInterval(executeIframe);
-        var iframeUrl = "https://o.lomadee.com/loc/session/";
-
-        var iframe         = document.createElement("iframe");
-        iframe.src         = iframeUrl + storeId + "?customerid=" + customerId;
-        iframe.id          = "iframe_mktplace";
-        iframe.width       = 0;
-        iframe.height      = 0;
-        iframe.frameheight = 0;
-        iframe.frameborder = 0;
-
-        console.log(iframe);
-        document.body.appendChild(iframe);
+(function ($) {
+  $(document).ready(function(){
+    var verifyDataLayer = function() {
+      if(typeof dataLayer === "undefined"){
+        return;
       }
-    }
-  };
 
-  var executeIframe = setInterval( verifyDataLayer , 3000 );
-});
+      var customerId = $("html").getAttribute("data-session");
+      var storeId    = document.scripts["mktplace"].getAttribute("data-store");
+
+      if(customerId){
+        var layer  = dataLayer[0];
+        var iframe = $('#iframe_mktplace');
+        if(iframe == null && layer.pageCategory === 'Carrinho'){
+          clearInterval(executeIframe);
+          var iframeUrl = "https://o.lomadee.com/loc/session/";
+
+          var iframe         = document.createElement("iframe");
+          iframe.src         = iframeUrl + storeId + "?customerid=" + customerId;
+          iframe.id          = "iframe_mktplace";
+          iframe.width       = 0;
+          iframe.height      = 0;
+          iframe.frameheight = 0;
+          iframe.frameborder = 0;
+
+          console.log(iframe);
+          document.body.appendChild(iframe);
+        }
+      }
+    };
+
+    var executeIframe = setInterval( verifyDataLayer , 3000 );
+  });
+})(jQuery);
