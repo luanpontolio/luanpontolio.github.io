35f935f802e7787c479ea7b47b4b656d341bd4d5
Luan Pontolio <luan.pontolio@gmail.com>
2017-06-01 15:11:43 -0300

    fixes script

--- iframe_buscape.js
+++ iframe_buscape.js
@@ -1,31 +1,32 @@
-var verifyDataLayer = function() {
-  (function(){
-    if(typeof dataLayer === "undefined"){
-      return;
+var verifyDataLayer = (function(){
+  if(typeof dataLayer === "undefined"){
+    return;
+  }
+
+  var customerId = document.querySelector("html").getAttribute("data-session");
+  var storeId    = document.scripts["mktplace"].getAttribute("data-store");
+
+  if(customerId){
+    var layer  = dataLayer[0];
+    var iframe = document.getElementById('iframe_mktplace');
+
+    console.log(iframe);
+    console.log(layer);
+    if(iframe == null && layer.visitorId && layer.pageCategory === 'Carrinho'){
+      clearInterval(executeIframe);
+      var iframeUrl = "https://o.lomadee.com/loc/session/";
+
+      var iframe         = document.createElement("iframe");
+      iframe.src         = iframeUrl + storeId + "?customerid=" + customerId;
+      iframe.id          = "iframe_mktplace";
+      iframe.width       = 0;
+      iframe.height      = 0;
+      iframe.frameheight = 0;
+      iframe.frameborder = 0;
+
+      document.body.appendChild(iframe);
     }
-
-    var customerId = document.querySelector("html").getAttribute("data-session");
-    var storeId    = document.scripts["mktplace"].getAttribute("data-store");
-
-    if(customerId){
-      var layer  = dataLayer[0];
-      var iframe = document.getElementById('iframe_mktplace');
-      if(iframe == null && layer.visitorId && layer.pageCategory === 'Carrinho'){
-        clearInterval(executeIframe);
-        var iframeUrl = "https://o.lomadee.com/loc/session/";
-
-        var iframe         = document.createElement("iframe");
-        iframe.src         = iframeUrl + storeId + "?customerid=" + customerId;
-        iframe.id          = "iframe_mktplace";
-        iframe.width       = 0;
-        iframe.height      = 0;
-        iframe.frameheight = 0;
-        iframe.frameborder = 0;
-
-        document.body.appendChild(iframe);
-      }
-    }
-  })();
-};
+  }
+})(document);
 
 var executeIframe = setInterval( verifyDataLayer , 3000 );
